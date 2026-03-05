c2705d59777c72c968f3b279befd5822606bbad2
Hans <thus.spoke.swan@gmail.com>
2026-03-05 19:18:28 +0530

    Revise comments in approx_FA module
    Updated module header comments for clarity and added author information.

--- rtl/approx_FA.v
+++ rtl/approx_FA.v
@@ -1,24 +1,18 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03.03.2026 23:48:46
-// Design Name: 
-// Module Name: approx_FA
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+//////////////////////////////////////////////////////////////////////////////////
+// 
+// Project Name: Approximate MAC using Wallace Tree Multiplier
+// Author: Hans Raj
+// Create Date: 03.03.2026 23:48:46
+// Module Name: approx_FA
+// Tools: Xilinx Vivado 
+// Description: Approximate full adder used in the Wallace tree reduction stage.
+// S = A | B | C
+// Cout = A & B
+// Used in LSB columns to reduce hardware complexity.
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
 module approx_FA(
     input wire A,
